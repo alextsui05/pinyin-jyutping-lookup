09fcb3f8118a6e950b940898410622ad4b5cb838
Alex Tsui <alextsui@pm.me>
2026-01-30 02:43:45 +0000

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import sampleData from "./assets/sample.json";
+
+type SnippetData = {
+  query: string;
+  jyutping: string;
+  pinyin: string;
+};
+
+function Snippet({ query, jyutping, pinyin }: SnippetData) {
+  return (
+    <div>
+      <p className="text-8xl font-bold">{query}</p>
+      <p>{jyutping}</p>
+      <p>{pinyin}</p>
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Chinese Snippets</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Snippet {...sampleData} />
     </>
   );
 }
