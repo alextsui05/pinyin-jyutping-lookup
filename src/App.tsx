bf4f29d16b7c9e135aed1fda57729ff0850a2965
Alex Tsui <alextsui@pm.me>
2026-01-31 03:25:19 +0000

    Add sample text

--- src/App.tsx
+++ src/App.tsx
@@ -139,6 +139,41 @@ function App() {
           </button>
         </div>
       </div>
+      <div className="text-left">
+        <p>
+          This is a tool to look up pinyin and jyutping for Chinese characters.
+          All you need to do is copy and paste in a snippet of Chinese in the
+          input box at the bottom of the page, and the Mandarin and Cantonese
+          transliterations will appear in a few seconds. You can also toggle
+          between simplified and traditional characters for each snippet. Feel
+          free to use the sample Chinese text:
+        </p>
+        <p className="mt-2 bg-gray-100 p-10">
+          基礎知識和術語 由於 Flexbox
+          是一個完整的模組，而不是單一屬性，因此它涉及許多方面，包括其所有屬性。其中一些屬性需要設定在容器（父元素，稱為「flex
+          容器」）上，而有些屬性則需要設定在子元素（稱為「flex 項目」）上。
+          如果說「常規」佈局是基於區塊級和行內兩種流方向，那麼彈性佈局則是基於「彈性流方向」。請參考規範中的這張圖，它解釋了彈性佈局背後的主要想法。
+          一張解釋 Flexbox 術語的圖表。 Flexbox
+          主軸方向上的尺寸稱為主尺寸，反方向的尺寸稱為交叉尺寸。這些尺寸都有主起始點、主結束點、交叉起始點和交叉結束點。
+          項目將按照main
+          axis（從main-start到main-end）或交叉軸（從cross-start到cross-end）進行佈局。
+          主軸–
+          彈性容器的主軸是彈性元素佈局的主要軸線。請注意，它不一定是水平的；這取決於屬性flex-direction（見下文）。
+          main-start | main-end – flex 專案放置在容器內，從 main-start 開始，到
+          main-end 結束。
+          主尺寸－彈性項目的寬度或高度（取其位於主要尺寸內的值）即為該項目的主尺寸。彈性項目的主尺寸屬性為「寬度」或「高度」屬性，取其位於主要尺寸內的值。
+          橫軸－垂直於主軸的軸稱為橫軸。它的方向取決於主軸的方向。 交叉起始 |
+          交叉結束–
+          彈性線填滿了項目，並從彈性容器的交叉起始側開始，向交叉結束側放置到容器中。
+          橫向尺寸–
+          彈性項目的寬度或高度（以橫向尺寸為準）即為該項目的橫向尺寸。橫向尺寸屬性的值取自“寬度”或“高度”中的“寬度”或“高度”。
+        </p>
+        <p className="mt-2 bg-gray-100 p-10">
+          对于算是推理类的avg而言，本作设定在怪谈类上不算独有创新，都结合开局类圣杯战争的大逃杀剧情后，诅咒和咒珠的设定便跃然纸上，让人拥有深刻的爽感与印象。整篇自圆其说，逻辑性不差，人物设定有趣，对话生动且关系环环相扣，多故事分支提高群像爽感与故事沉浸了解度。本人智商不高，简单看过资料与回顾对话后也能算是理解故事全篇意味，挑不出故事中的漏洞或错处牵强，姑且如此认为。
+          本篇序章可能是最吓人的，有一些jumpscare（别的有但不多，有心理准备）但过了杀杀杀环节后就没有火力不足的恐惧了，后面都是故事的推进，诅咒者杀心不重且对其效果有了理解，氛围也因为角色间对话的诙谐与打趣而变得不那么沉重或恐怖。基本上每一组主视角双组合都有缓解气氛的小太阳担当，不必因为害怕不敢玩：）加油加油
+        </p>
+      </div>
+      <p className="text-left"></p>
       {transliterations.map((snippet, idx) => (
         <Snippet
           key={idx}
@@ -156,7 +191,11 @@ function App() {
           placeholder="Enter Chinese text"
           className="mr-4"
         />
-        <button type="submit" disabled={isLoading}>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+          type="submit"
+          disabled={isLoading}
+        >
           {isLoading ? (
             <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-gray-900"></div>
           ) : (
